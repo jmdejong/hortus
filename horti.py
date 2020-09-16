92ffcc25f6d4c87e2afcbf020a7c7c431c358d2f
troido <troido@protonmail.com>
2020-09-16 22:06:14 +0200

    fix dying calculation; add visitors; order plants by last watered

--- horti.py
+++ horti.py
@@ -5,6 +5,7 @@ import json
 import sys
 from datetime import datetime
 import textwrap
+import time
 
 # returns the input string with the specified length by cropping and padding
 # the padding parameter should be a single character
@@ -14,7 +15,46 @@ def setStrLen(string, length, padding=' '):
         string += padding*(length-len(string))
     return string
 
+def loadJsonFile(fname):
+    try:
+        with open(fname) as f:
+            try:
+                data = json.load(f)
+            except json.decoder.JSONDecodeError as e:
+                return None
+    except OSError as e:
+        return None
+    return data
 
+def calculateLastWatered(last_watered, visitors):
+    guest_timestamps = []
+    for visitor in visitors:
+        if not isinstance(visitor, dict):
+            continue
+        timestamp = visitor.get("timestamp")
+        if not isinstance(timestamp, int):
+            continue
+        if timestamp <= int(time.time()):
+            guest_timestamps.append(timestamp)
+    
+    all_timestamps = [last_watered] + guest_timestamps
+    all_timestamps.sort()
+    # calculate # of days between each guest watering
+    timestamp_diffs = [(j-i)/86400.0 for i, j in zip(all_timestamps[:-1], all_timestamps[1:])]
+    # plant's latest timestamp should be set to last timestamp before a
+    # gap of 5 days
+    # TODO: this considers a plant watered only on day 1 and day 4 to be
+    # watered for all 4 days - need to figure out how to only add score
+    # from 24h after each watered timestamp
+    last_valid_element = next((x for x in timestamp_diffs if x > 5), None)
+    if not last_valid_element:
+        # all timestamps are within a 5 day range, can just use latest one
+        return all_timestamps[-1]
+    last_valid_index = timestamp_diffs.index(last_valid_element)
+    # slice list to only include up until a >5 day gap
+    valid_timestamps = all_timestamps[:last_valid_index + 1]
+    return valid_timestamps[-1]
+    
 
 class Horti:
     
@@ -57,25 +97,32 @@ class Horti:
         
         return art
     
-    def getPlotLinesFromJson(self, filename):
-        try:
-            with open(filename) as f:
-                try:
-                    data = json.load(f)
-                except json.decoder.JSONDecodeError:
-                    return None
-        except OSError:
+    def getPlotLinesFromJson(self, user):
+        plantpath = os.path.join(self.config['userdir'], user, self.config['plantdata'].format(user=user))
+        data = loadJsonFile(plantpath)
+        if not isinstance(data, dict):
+            return None
+        visitorpath = os.path.join(self.config['userdir'], user, self.config['visitordata'])
+        visitors = loadJsonFile(visitorpath)
+        if not isinstance(visitors, list):
+            visitors = []
+        
+        
+        last_watered = data.get("last_watered")
+        description = data.get("description")
+        owner = data.get("owner")
+        is_dead = data.get('is_dead', False)
+        if not (isinstance(last_watered, int) and isinstance(description, str) and isinstance(owner, str) and isinstance(is_dead, bool)):
             return None
         
-        # based on archangelic's code for pinhook
-        # https://github.com/archangelic/pinhook-tilde/blob/master/plugins/watered.py
-        if "last_watered" in data:
-            last_watered = datetime.utcfromtimestamp(data['last_watered'])
-            water_diff = datetime.now() - last_watered
-        else:
-            water_diff = 0
+        last_watered = calculateLastWatered(last_watered, visitors)
         
-        art = self.getArt(data['description'], data.get('is_dead', False) and water_diff.days >= 5)
+        time_delta_watered = int(time.time()) - last_watered
+        if time_delta_watered > (5 * (24 * 3600)):
+            is_dead = True
+        if user == "abraxas":
+            is_dead = False
+        art = self.getArt(description, is_dead)
         
         width = self.config['plotwidth']
         
@@ -96,17 +143,16 @@ class Horti:
             line = setStrLen(line, width)
             plotlines.append(line)
         
-        return plotlines
+        return plotlines, last_watered
     
     def loadPlantData(self):
         allData = []
         for user in os.listdir(self.config['userdir']):
-            plantpath = os.path.join(self.config['userdir'], user, self.config['plantdata'].format(user=user))
-            if os.path.isfile(plantpath):
-                plotlines = self.getPlotLinesFromJson(plantpath)
-                if plotlines:
-                    allData.append(plotlines)
-        return allData
+            plantData = self.getPlotLinesFromJson(user)
+            if plantData:
+                allData.append(plantData)
+        allData.sort(key=lambda d: -d[1])
+        return [d[0] for d in allData]
     
     def toString(self, data):
         
